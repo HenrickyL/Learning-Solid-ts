f8a9781663eda72ecd716ec451d9a3e86dbbc2b4
henrickyL <henrickyl1@gmail.com>
2024-10-15 21:55:35 -0300

    router

--- src/routers.ts
+++ src/routers.ts
@@ -1,9 +1,14 @@
 import { Router, Request, Response } from 'express';
+import { adaptRoute } from './_infra/adapters/ExpressRouterAdapter';
+import { CreateUserController } from './_useCases/CreateUser/CreateUserController';
+import { CreateUserUseCase } from './_useCases/CreateUser/CreateUserUseCase';
+import { UserCreateRequest, UserCreateResponse } from './_useCases/CreateUser/CreateUserDTO';
 
 const router = Router();
+// create IUserRepository
+const createUserUC = new CreateUserUseCase(null);
+const userController = new CreateUserController(createUserUC);
 
-router.post('/users', (req: Request, res: Response) => {
-    return res.status(201).send();
-});
+router.post('/users', adaptRoute<UserCreateRequest,UserCreateResponse>(userController));
 
 export { router }
